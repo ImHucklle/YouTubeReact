f288d33e8027b0be66d15e4606b012c9c5224d88
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-11-09 12:34:57 -0500

    Finished the setup process for the React App

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <h1>YouTube Main Component</h1>
+        )
+    }
+}
+
+export default App;
