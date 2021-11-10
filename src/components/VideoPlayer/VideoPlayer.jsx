07a60d83e3516bb4a6a84662123a43d68d5f3c81
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2021-11-10 16:38:02 -0600

    Added comment.

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -5,7 +5,7 @@ const VideoPlayer = ({ video_id }) => (
     <body>
         <div className="video-responsive">
             <iframe id="ytplayer" type="text/html" width="640" height="360"
-            src={`https://www.youtube.com/embed/${video_id}`}
+            src={`https://www.youtube.com/embed/${video_id}`} // ` back tik is used to read the variable
             frameborder="0"></iframe>
         </div>
     </body>    
