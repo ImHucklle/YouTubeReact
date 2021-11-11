994b86478820170d6151e2973d23d24d682b14e2
Luca Leyva <imlucaleyva@gmail.com>
2021-11-11 15:43:30 -0500

    Styled RecommendedVideos and VideoCard components.

--- src/components/RecommendedVideos/RecommendedVideos.jsx
+++ src/components/RecommendedVideos/RecommendedVideos.jsx
@@ -6,15 +6,72 @@ function RecommendedVideos(){
     return(
         <div className="recommendedVideos">
             <h2>Recommended</h2>
-            <div className="recommendedVideos__videos"></div>
-            <VideoCard 
-                title="THE BATMAN - Main Trailer"
-                views="33.3 Million"
-                timestamp="1 month ago"
-                channelImage="https://i.ytimg.com/an_webp/mqqft2x_Aa4/mqdefault_6s.webp?du=3000&sqp=CKK6tYwG&rs=AOn4CLDDV8m43H4nyN4I9uQwupreMNHSPA"
-                channel="Warner Brothers Pictures"
-                image="https://yt3.ggpht.com/ytc/AKedOLRF9_MOYUXfJ-8Bxknj0YSma7RNmZ6TU9jInnpxvg=s176-c-k-c0x00ffffff-no-rj"
-            />
+            <div className="recommendedVideos__videos">
+                <VideoCard 
+                    title="THE BATMAN - Main Trailer"
+                    views="33.3 Million"
+                    timestamp="1 month ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLRF9_MOYUXfJ-8Bxknj0YSma7RNmZ6TU9jInnpxvg=s176-c-k-c0x00ffffff-no-rj"
+                    channel="Warner Brothers Pictures"
+                    image="https://i.ytimg.com/an_webp/mqqft2x_Aa4/mqdefault_6s.webp?du=3000&sqp=CKK6tYwG&rs=AOn4CLDDV8m43H4nyN4I9uQwupreMNHSPA"
+                />
+                <VideoCard 
+                    title="THE BATMAN - Main Trailer"
+                    views="33.3 Million"
+                    timestamp="1 month ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLRF9_MOYUXfJ-8Bxknj0YSma7RNmZ6TU9jInnpxvg=s176-c-k-c0x00ffffff-no-rj"
+                    channel="Warner Brothers Pictures"
+                    image="https://i.ytimg.com/an_webp/mqqft2x_Aa4/mqdefault_6s.webp?du=3000&sqp=CKK6tYwG&rs=AOn4CLDDV8m43H4nyN4I9uQwupreMNHSPA"
+                />
+                <VideoCard 
+                    title="THE BATMAN - Main Trailer"
+                    views="33.3 Million"
+                    timestamp="1 month ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLRF9_MOYUXfJ-8Bxknj0YSma7RNmZ6TU9jInnpxvg=s176-c-k-c0x00ffffff-no-rj"
+                    channel="Warner Brothers Pictures"
+                    image="https://i.ytimg.com/an_webp/mqqft2x_Aa4/mqdefault_6s.webp?du=3000&sqp=CKK6tYwG&rs=AOn4CLDDV8m43H4nyN4I9uQwupreMNHSPA"
+                />
+                <VideoCard 
+                    title="THE BATMAN - Main Trailer"
+                    views="33.3 Million"
+                    timestamp="1 month ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLRF9_MOYUXfJ-8Bxknj0YSma7RNmZ6TU9jInnpxvg=s176-c-k-c0x00ffffff-no-rj"
+                    channel="Warner Brothers Pictures"
+                    image="https://i.ytimg.com/an_webp/mqqft2x_Aa4/mqdefault_6s.webp?du=3000&sqp=CKK6tYwG&rs=AOn4CLDDV8m43H4nyN4I9uQwupreMNHSPA"
+                />
+                <VideoCard 
+                    title="THE BATMAN - Main Trailer"
+                    views="33.3 Million"
+                    timestamp="1 month ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLRF9_MOYUXfJ-8Bxknj0YSma7RNmZ6TU9jInnpxvg=s176-c-k-c0x00ffffff-no-rj"
+                    channel="Warner Brothers Pictures"
+                    image="https://i.ytimg.com/an_webp/mqqft2x_Aa4/mqdefault_6s.webp?du=3000&sqp=CKK6tYwG&rs=AOn4CLDDV8m43H4nyN4I9uQwupreMNHSPA"
+                />
+                <VideoCard 
+                    title="THE BATMAN - Main Trailer"
+                    views="33.3 Million"
+                    timestamp="1 month ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLRF9_MOYUXfJ-8Bxknj0YSma7RNmZ6TU9jInnpxvg=s176-c-k-c0x00ffffff-no-rj"
+                    channel="Warner Brothers Pictures"
+                    image="https://i.ytimg.com/an_webp/mqqft2x_Aa4/mqdefault_6s.webp?du=3000&sqp=CKK6tYwG&rs=AOn4CLDDV8m43H4nyN4I9uQwupreMNHSPA"
+                />
+                <VideoCard 
+                    title="THE BATMAN - Main Trailer"
+                    views="33.3 Million"
+                    timestamp="1 month ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLRF9_MOYUXfJ-8Bxknj0YSma7RNmZ6TU9jInnpxvg=s176-c-k-c0x00ffffff-no-rj"
+                    channel="Warner Brothers Pictures"
+                    image="https://i.ytimg.com/an_webp/mqqft2x_Aa4/mqdefault_6s.webp?du=3000&sqp=CKK6tYwG&rs=AOn4CLDDV8m43H4nyN4I9uQwupreMNHSPA"
+                />
+                <VideoCard 
+                    title="THE BATMAN - Main Trailer"
+                    views="33.3 Million"
+                    timestamp="1 month ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AKedOLRF9_MOYUXfJ-8Bxknj0YSma7RNmZ6TU9jInnpxvg=s176-c-k-c0x00ffffff-no-rj"
+                    channel="Warner Brothers Pictures"
+                    image="https://i.ytimg.com/an_webp/mqqft2x_Aa4/mqdefault_6s.webp?du=3000&sqp=CKK6tYwG&rs=AOn4CLDDV8m43H4nyN4I9uQwupreMNHSPA"
+                />
+            </div>
         </div>
     )
 }
--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,16 +1,17 @@
-import Avatar from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
 import React from 'react';
+import './VideoCard.css';
 
 function VideoCard({ image, title, channel, views, timestamp, channelImage}) {
     return (
         <div className="videoCard">
-            <img src={image} alt="" />
+            <img className="videoCard__thumbnail" src={image} alt="" />
             <div className="videoCard_info">
                 <Avatar 
                     className="videoCard_avatar"
                     alt={channel}
                     src={channelImage} />
-                <div className="video__text">
+                <div className="videoCard__text">
                     <h4>{title}</h4>
                     <p>{channel}</p>
                     <p>
