0d9919a79f1ce95f576b341f780ebd7a88246202
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2021-11-12 16:40:24 -0600

    Added styling to the message scroll created t message component

--- src/components/CommentSection/CommentSectionCopenents/Message/Message.js
+++ src/components/CommentSection/CommentSectionCopenents/Message/Message.js
@@ -1,10 +1,66 @@
-import React from 'react';
-
+import React, {useRef, useState} from 'react';
+import './Message.css'
 function Message(props){
-    return(
-        <div>
 
-        </div>
+
+    const likeIcon = useRef();
+    const numLikes = useRef();
+
+    const[arrowUp, setArrowUp] = useState(false);
+    const[openReply, setOpenReply] = useState(false);
+
+    //when canel/reply button  press
+
+    const changeOpenReply=() =>{
+        setOpenReply(prevState=> prevState = !prevState)
+    }
+    // toggle arrow up down
+    let arrow = <i className='fas fa-caret-down'></i>
+
+    const changeArrow= () => {
+        setArrowUp(prevState=> prevState = !prevState)
+    }
+
+    if(arrowUp){
+        arrow = <i className='fas fa-caret-up'></i>
+    }
+    else{
+        arrow = <i className='fas fa-caret-down'></i>
+    }
+
+    const likeComment = () => {}
+
+    const deleteMessage = () => {}
+
+
+
+
+    return(
+        <>
+        <section className='messageContainer'>
+            <div className='messageUser'>{props.user}</div>
+            <i className='fas fa-user-cirle'></i>
+            <div className='messageText'>{props.message}</div>
+            <section className='messageIconsContainer'>
+                <i className='fas fa-thumbs-up'></i>
+                <div ref = {numLikes}>{props.likes}</div>
+                <i className='fas fa-thumbs-down'></i>
+                {/* {
+                    !props.editable ? (
+                        <div onClick={changeOpenReply}
+                        style={{cursor:'pointer'}}>REPLY</div>
+                    ) : (
+                        <div onClick={deleteMessage}
+                        style={{cursor: "pointer"}}>DELETE</div>
+                    )
+                } */}
+            </section>
+            <section className='arrowReplies' onClick={changeArrow}>
+                {arrow}
+                <div>View replies</div>
+            </section>
+        </section>
+        </>
     )
 }
 
--- src/components/CommentSection/MessageScroll.js
+++ src/components/CommentSection/MessageScroll.js
@@ -4,7 +4,7 @@ import Message from './CommentSectionCopenents/Message/Message'
 function MessageScroll(props){
     return ( 
         <>
-        <Message />
+        <Message user='Anonymous' editable={false} message = 'NICE VID' likes ={25}/>
         <div className='bottomBar'>
         <div className = 'loader'/>
         </div>
